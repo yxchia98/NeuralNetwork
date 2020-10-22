e73af2d1038a368510ab3ad1db757ce6f45f3b11
Lulmao <70194529+yxchia98@users.noreply.github.com>
2020-10-22 15:01:20 +0800

    Merged read_attr into read_txt
    Merged the 2 functions into 1
    strtok wont modifiy the original txt_array anymore

--- test.c
+++ test.c
@@ -2,28 +2,28 @@
 #include <math.h>
 #include <stdlib.h>
 #include <string.h>
-#define TXT_LINE_SIZE 45                    //maximum length of text string per line
+#define TXT_LINE_SIZE 41                    //maximum number of chars in per line in .txt file
 #define SIZE 100                            //size of dataset
-void read_txt(char* filename, char c[TXT_LINE_SIZE], char txt_array[SIZE][TXT_LINE_SIZE]);
-void read_attr(char txt_array[SIZE][TXT_LINE_SIZE], float season[], float age[], float alcFreq[], float sithour[], int disease[], int acci[], int surgInt[], int fever[], int smoke[], int semenDiag[]);
+void read_txt(char* filename, char c[TXT_LINE_SIZE], char txt_array[SIZE][TXT_LINE_SIZE], float season[], float age[], float alcFreq[], float sitHour[], int disease[], int acci[], int surgInt[], int fever[], int smoke[], int semenDiag[]);
 void read_floatArray(float array[]);
 void read_intArray(float array[]);
 int main()
 {
-    static float season[SIZE], age[SIZE]={}, alcFreq[SIZE], sitHour[SIZE];    // arrays for attributes with float data types
+    static float season[SIZE], age[SIZE], alcFreq[SIZE], sitHour[SIZE];    // arrays for attributes with float data types
     static int disease[SIZE], acci[SIZE], surgInt[SIZE], fever[SIZE], smoke[SIZE], semenDiag[SIZE]; // arrays for attributes with int data types
     char c[TXT_LINE_SIZE];
-    char txt_array[SIZE][TXT_LINE_SIZE];
+    char txt_array[SIZE][TXT_LINE_SIZE]={};
     char* filename="fertility_Diagnosis_Data_Group1_4.txt";
-    read_txt(filename, c, txt_array);               // reads txt file and assigns it into txt_array
-    read_attr(txt_array, season, age, alcFreq, sitHour, disease, acci, surgInt, fever, smoke, semenDiag);
+    read_txt(filename, c, txt_array, season, age, alcFreq, sitHour, disease, acci, surgInt, fever, smoke, semenDiag);               // reads txt file and assigns it into txt_array
     return 0;
 }
 
-void read_txt(char* filename, char c[TXT_LINE_SIZE], char txt_array[SIZE][TXT_LINE_SIZE])
+void read_txt(char* filename, char c[TXT_LINE_SIZE], char txt_array[SIZE][TXT_LINE_SIZE], float season[], float age[], float alcFreq[], float sitHour[], int disease[], int acci[], int surgInt[], int fever[], int smoke[], int semenDiag[])
 {
-    int i=0;
+    int i=0,k;
     FILE *fp;                               //file pointer
+    char *ptr;
+    char delim[]=",";
     fp=fopen(filename,"r");                 //open file, read only
     if(fp==NULL)
     {
@@ -33,24 +33,8 @@ void read_txt(char* filename, char c[TXT_LINE_SIZE], char txt_array[SIZE][TXT_LI
     while(fgets(c,TXT_LINE_SIZE,fp)!=NULL)
     {
         strcpy(txt_array[i],c);             //Copy line from .txt file into string array
-        //printf("Line %d: ",i+1);
-        //printf("%s",c);
-        i++;
-    }
-    fclose(fp);
-
-}
-
-void read_attr(char txt_array[SIZE][TXT_LINE_SIZE], float season[], float age[], float alcFreq[], float sitHour[], int disease[], int acci[], int surgInt[], int fever[], int smoke[], int semenDiag[])
-{
-    int i=0,k;
-    char delim[]=",";
-    char *ptr;
-    for(i=0;i<100;i++)
-    {
         k=0;
-        //printf(" String in txt_array[%d]: %s",i,txt_array+i);
-        ptr=strtok(txt_array[i],delim);
+        ptr=strtok(c,delim);
         while(ptr!=NULL)
         {
             switch (k)
@@ -62,36 +46,39 @@ void read_attr(char txt_array[SIZE][TXT_LINE_SIZE], float season[], float age[],
                 age[i]=atof(ptr);
                 break;
                 case 2:
-                disease[i]=atoll(ptr);
+                disease[i]=atoi(ptr);
                 break;
                 case 3:
-                acci[i]=atoll(ptr);
+                acci[i]=atoi(ptr);
                 break;
                 case 4:
-                surgInt[i]=atoll(ptr);
+                surgInt[i]=atoi(ptr);
                 break;
                 case 5:
-                fever[i]=atoll(ptr);
+                fever[i]=atoi(ptr);
                 break;
                 case 6:
                 alcFreq[i]=atof(ptr);
                 break;
                 case 7:
-                smoke[i]=atoll(ptr);
+                smoke[i]=atoi(ptr);
                 break;
                 case 8:
                 sitHour[i]=atof(ptr);
                 break;
                 case 9:
-                semenDiag[i]=atoll(ptr);
+                semenDiag[i]=atoi(ptr);
                 break;
             }
-            //printf(" ATTR%d: %s",k+1,ptr);
             k++;
             ptr=strtok(NULL,delim);
         }
 
+
+        i++;
     }
+    fclose(fp);
+
 }
 
 void read_floatArray(float array[])
