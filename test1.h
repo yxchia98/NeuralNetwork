f76d67729d4d352bb06acdc88edf64e4055d53af
Lulmao <70194529+yxchia98@users.noreply.github.com>
2020-10-31 15:35:28 +0800

    Update test1.h

--- test1.h
+++ test1.h
@@ -4,6 +4,11 @@
 #include <string.h>
 #define TXT_LINE_SIZE 41                    //maximum number of chars in per line in .txt file
 #define SIZE 100                            //size of dataset
+void read_txt(char* filename, char c[TXT_LINE_SIZE], char txt_array[SIZE][TXT_LINE_SIZE], double season[], double age[], double alcFreq[], double sitHour[], int disease[], int acci[], int surgInt[], int fever[], int smoke[], int semenDiag[]);
+void randWeight(double x[], int n);
+double randomize(double min, double max);
+void read_floatArray(float array[]);
+void read_intArray(float array[]);
 
 //Reads from the .txt dataset file and assigns values in it into the corresponding attribute arrays
 void read_txt(char* filename, char c[TXT_LINE_SIZE], char txt_array[SIZE][TXT_LINE_SIZE], double season[], double age[], double alcFreq[], double sitHour[], int disease[], int acci[], int surgInt[], int fever[], int smoke[], int semenDiag[])
@@ -69,7 +74,17 @@ void read_txt(char* filename, char c[TXT_LINE_SIZE], char txt_array[SIZE][TXT_LI
 
 }
 
-double randfrom(double min, double max) 
+void randWeight(double x[],int n)
+{
+    int i;
+    printf("Size of array in bytes:%d",sizeof(x));
+    for(i=0;i<n;i++)
+    {
+        x[i]=randomize(-1.0,1.0);
+    }
+}
+
+double randomize(double min, double max) 
 {
     double range = (max - min); 
     double div = RAND_MAX / range;
