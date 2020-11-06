60ddc1192deee86be0bae996f59b1a4d22b00121
Yong Ting <32158771+yongting1998@users.noreply.github.com>
2020-11-06 18:31:25 +0800

    update yongting

--- yongting.h
+++ yongting.h
@@ -5,11 +5,7 @@ double linear_regression(double arrayX[9], double arrayW[9], double bias);
 double linear_regression(double arrayX[9], double arrayW[9], double bias){
     double sum;
     for(int i =0; i<9; i++){
-        printf("\n arrayX%d %f", i, arrayX[i]);
-        printf("\n arrayW%d %f", i, arrayW[i]);
         sum += arrayX[i] * arrayW[i];
     }
-    printf("\nhit");
-    printf("this is the value %f", sum);
     return (sum + bias);
 }
