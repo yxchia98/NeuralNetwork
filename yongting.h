ce273a9c9e11d8175647d20dc0dc37e6c31681c0
Yong Ting <32158771+yongting1998@users.noreply.github.com>
2020-11-10 21:01:44 +0800

    fixed bias formula error

--- yongting.h
+++ yongting.h
@@ -2,17 +2,17 @@
 #include <math.h>
 
 double linear_regression(double arrayX[9], double arrayW[9], double bias);
-double backward_propogation(double MAE, float x, double z);
+double backward_propogation(double current_error, double z, float x);
 double linear_regression(double arrayX[9], double arrayW[9], double bias)
 {
     double sum;
     for (int i = 0; i < 9; i++)
     {
-        sum += arrayX[i] * arrayW[i];
+        sum += arrayX[i] * arrayW[i] + bias;
     }
-    return (sum + bias);
+    return sum;
 }
-double backward_propogation(double MAE, float x, double linear_regression_val)
+double backward_propogation(double current_error, double linear_regression_val, float x)
 {
-    return (MAE * (exp(linear_regression_val) / pow(1 + exp(linear_regression_val), 2)) * x);
+    return (current_error * (exp(linear_regression_val) / pow(1 + exp(linear_regression_val), 2)) * x);
 }
