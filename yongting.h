6a40220fcddd3f4e4460e6ca3ea050c0e42ddefd
Lulmao <70194529+yxchia98@users.noreply.github.com>
2020-11-12 14:28:53 +0800

    update bias

--- yongting.h
+++ yongting.h
@@ -8,9 +8,9 @@ double linear_regression(double arrayX[9], double arrayW[9], double bias)
     double sum;
     for (int i = 0; i < 9; i++)
     {
-        sum += arrayX[i] * arrayW[i] + bias;
+        sum += arrayX[i] * arrayW[i];
     }
-    return sum;
+    return sum + bias;
 }
 double backward_propogation(double current_error, double linear_regression_val, float x)
 {
