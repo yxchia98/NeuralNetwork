be440e2907260f7f041bb4f4daa24eaffc116dfe
Yong Ting <32158771+yongting1998@users.noreply.github.com>
2020-11-29 02:24:08 +0800

    BOLD TEXT

--- confusionMatrix.c
+++ confusionMatrix.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include "assignment.h"
+#define BOLDBLACK "\033[1m\033[30m" /* Bold Black */
+#define RESET "\033[0m"
 //1 = TP, 2 = FP, 3 = TN, 4 = FN
 // int confusionCount[4][4];
 // confusionCount[0][0] = 0;
@@ -46,12 +48,19 @@ void confusionMatrix(int confusionCount[4], float predictedY, int output)
 }
 void printConfusionMatrix(int confusionCount[4][4], double MMSE[4])
 {
-    printf("\n\nConfusion Matrix for 90 training dataset before training\nTrue Positive  : %d \nFalse Positive : %d \nTrue Negative  : %d \nFalse Negative : %d ", confusionCount[0][0], confusionCount[0][1], confusionCount[0][2], confusionCount[0][3]);
+    printf(BOLDBLACK "\n\nConfusion Matrix for 90 training dataset before training" RESET);
+    printf("\nTrue Positive  : %d \nFalse Positive : %d \nTrue Negative  : %d \nFalse Negative : %d " RESET, confusionCount[0][0], confusionCount[0][1], confusionCount[0][2], confusionCount[0][3]);
     printf("\nMMSE of testing dataset: %lf", MMSE[0]);
-    printf("\n\nConfusion Matrix for 10 testing dataset before training\nTrue Positive  : %d \nFalse Positive : %d \nTrue Negative  : %d \nFalse Negative : %d ", confusionCount[1][0], confusionCount[1][1], confusionCount[1][2], confusionCount[1][3]);
+
+    printf(BOLDBLACK "\n\nConfusion Matrix for 90 training dataset before training" RESET);
+    printf("\nTrue Positive  : %d \nFalse Positive : %d \nTrue Negative  : %d \nFalse Negative : %d ", confusionCount[1][0], confusionCount[1][1], confusionCount[1][2], confusionCount[1][3]);
     printf("\nMMSE of testing dataset: %lf", MMSE[1]);
-    printf("\n\nConfusion Matrix for 90 training dataset after training\nTrue Positive  : %d \nFalse Positive : %d \nTrue Negative  : %d \nFalse Negative : %d ", confusionCount[2][0], confusionCount[2][1], confusionCount[2][2], confusionCount[2][3]);
+
+    printf(BOLDBLACK "\n\nConfusion Matrix for 90 training dataset before training" RESET);
+    printf("\nTrue Positive  : %d \nFalse Positive : %d \nTrue Negative  : %d \nFalse Negative : %d ", confusionCount[2][0], confusionCount[2][1], confusionCount[2][2], confusionCount[2][3]);
     printf("\nMMSE of testing dataset: %lf", MMSE[2]);
-    printf("\n\nConfusion Matrix for 10 testing dataset after training\nTrue Positive  : %d \nFalse Positive : %d \nTrue Negative  : %d \nFalse Negative : %d ", confusionCount[3][0], confusionCount[3][1], confusionCount[3][2], confusionCount[3][3]);
+
+    printf(BOLDBLACK "\n\nConfusion Matrix for 90 training dataset before training" RESET);
+    printf("\nTrue Positive  : %d \nFalse Positive : %d \nTrue Negative  : %d \nFalse Negative : %d ", confusionCount[3][0], confusionCount[3][1], confusionCount[3][2], confusionCount[3][3]);
     printf("\nMMSE of testing dataset: %lf", MMSE[3]);
 }
